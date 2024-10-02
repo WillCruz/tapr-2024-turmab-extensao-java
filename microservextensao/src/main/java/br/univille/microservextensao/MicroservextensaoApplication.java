8ffe0c2f042b8506eed4ac13dec780197d9be596
WillCruz <will_willian956@hotmail.com>
2024-10-02 00:53:56 +0000

    Criação do Projeto Java

---
+++ microservextensao/src/main/java/br/univille/microservextensao/MicroservextensaoApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microservextensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservextensaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservextensaoApplication.class, args);
+	}
+
+}
---
+++ microservextensao/src/test/java/br/univille/microservextensao/MicroservextensaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microservextensao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservextensaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
