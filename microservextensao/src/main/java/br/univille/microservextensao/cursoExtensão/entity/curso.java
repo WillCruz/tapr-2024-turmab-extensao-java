d71f5f4d2a810c4278716c3115c7a7d36ded0d00
WillCruz <will_willian956@hotmail.com>
2024-10-09 23:17:43 +0000

    Aula 09-10

--- microservextensao/src/main/java/br/univille/microservextensao/cursoExtensão/entity/curso.java
+++ microservextensao/src/main/java/br/univille/microservextensao/cursoExtensão/entity/curso.java
@@ -1,14 +1,20 @@
 package br.univille.microservextensao.entidades;
 
+
 import java.util.UUID;
 
-@Entity
 
+@Container(containerName = "curso", autoCreateCintainer = true)
 public class curso {
-
+    @Id
+    @PartitionKey
+    @GeneratedValue
     private UUID id;
+
     private String curso;
+
     private String status;
+    
     private String disciplinas;
     
     
