2aadc10d4842ee08e235de6b1d6e3e1c03048c87
WillCruz <will_willian956@hotmail.com>
2024-10-09 00:56:10 +0000

    Aula 08-10

--- microservextensao/src/main/java/br/univille/microservextensao/cursoExtensão/entity/curso.java
+++ microservextensao/src/main/java/br/univille/microservextensao/cursoExtensão/entity/curso.java
@@ -1,16 +1,14 @@
 package br.univille.microservextensao.entidades;
 
+import java.util.UUID;
+
 @Entity
 
 public class curso {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
+    private UUID id;
     private String curso;
-
     private String status;
-
     private String disciplinas;
     
     
